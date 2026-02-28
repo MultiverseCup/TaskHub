3a24be8b8a852f094205dffa60294b0f16f32781
MultiverseCup <artur.gareev.06.10@gmail.com>
2026-02-28 12:32:48 +0500

    Added response time and student headers middleware

---
+++ TaskHub/Api/Middleware/ResponseTimeMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace Api.Middleware;
+
+public class ResponseTimeMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ResponseTimeMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        context.Response.OnStarting(() =>
+        {
+            stopwatch.Stop();
+            var elapsedMs = stopwatch.ElapsedMilliseconds;
+            context.Response.Headers["X-Response-Time-Ms"] = elapsedMs.ToString();
+            return Task.CompletedTask;
+        });
+
+        await _next(context);
+    }
+}
---
+++ TaskHub/Api/Middleware/StudentHeadersMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace Api.Middleware;
+
+public class StudentHeadersMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public StudentHeadersMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Response.OnStarting(() =>
+        {
+            context.Response.Headers["X-Student-Name"] = "Gareyev Artur Robertovich";
+            context.Response.Headers["X-Student-Group"] = "RI-240930";
+            return Task.CompletedTask;
+        });
+
+        await _next(context);
+    }
+}
